62106b2ef6d0a064b97eca31bc83e59c88d3328a
8c6t <nkw8620@gmail.com>
2020-04-15 20:17:01 +0900

    추가: 회원 가입 로직

--- src/sagas/auth.js
+++ src/sagas/auth.js
@@ -1,10 +1,12 @@
 import { takeLatest } from 'redux-saga/effects';
 import createRequestSaga from '../lib/createRequestSaga';
-import { LOGIN } from '../reducers/auth';
+import { LOGIN, SIGNUP } from '../reducers/auth';
 import * as authAPI from '../lib/api/auth';
 
 const loginSaga = createRequestSaga(LOGIN, authAPI.signIn);
+const signUpSaga = createRequestSaga(SIGNUP, authAPI.signUp);
 
 export default function* authSaga() {
   yield takeLatest(LOGIN, loginSaga);
+  yield takeLatest(SIGNUP, signUpSaga);
 }
