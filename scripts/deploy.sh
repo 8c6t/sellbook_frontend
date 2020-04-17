50f2dc74874adab270103e7f8a91da5ae5c1a811
8c6t <nkw8620@gmail.com>
2020-04-17 23:22:41 +0900

    수정: 압축 해제 권한

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,7 +6,7 @@ echo "> 이전 파일 삭제"
 sudo rm -rf $REPOSITORY/frontend/*
 
 echo "> 빌드 파일 압축 해제"
-unzip $REPOSITORY/deploy/frontend/build.zip -d $REPOSITORY/frontend
+sudo unzip $REPOSITORY/deploy/frontend/build.zip -d $REPOSITORY/frontend
 
 echo "> Nginx reload"
 sudo service nginx reload
